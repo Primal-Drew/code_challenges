2f7c7bd0cd00a4902e4d14187f8d1e03bad52965
primal-drew <oginaandrew05@gmail.com>
2024-03-28 10:45:40 +0300

    DAY 6: Break camelCase

---
+++ Intermediate/Break camelCase/README.md
@@ -0,0 +1,19 @@
+# BREAK camelCase
+## Description
+THE [LINK](https://www.codewars.com/kata/5208f99aee097e6552000148/train/c)
+
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+``Example``
+
+```
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+```
+
+## LEARNING RESOURCES
+With how I did my solution you need to know:
+ ``` go
+    1.   isupper()- check the man pages
+```
---
+++ Intermediate/Break camelCase/solution.c
@@ -0,0 +1,34 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//returned buffer should be dynamically allocated and will be freed by a caller
+char* solution(const char *camelCase) 
+{
+    size_t len = strlen(camelCase);
+    char* broken_camelCase = malloc(len+10);
+    // Copying values one by one!
+    int moves=0;
+    int indx =0;
+    broken_camelCase[indx++] = camelCase[0];
+    printf("Value of indx after 0: %i\n",indx);
+    for(int i=1;i<len+moves;i++)
+    {
+        if(isupper(camelCase[i]))
+        {
+            broken_camelCase[indx++] = ' ';
+            broken_camelCase[indx++] = camelCase[i];
+            continue;
+        }
+            broken_camelCase[indx++] = camelCase[i];
+    }
+    broken_camelCase[indx]  = '\0';
+    return broken_camelCase;
+}
+
+int main()
+{
+    const char* camelCase = "AndrewOgInaTyconMyGod";
+    printf("The Result: %s\n",solution(camelCase));
+}
